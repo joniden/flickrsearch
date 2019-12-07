170b485963520655f0f5e9c78181d74b9d894639
Joacim Niden <joacim.niden@mobiento.com>
2019-12-07 17:12:18 +0100

    Initial Commit

---
+++ FlickrSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlickrSearch
+//
+//  Created by Joacim Nidén on 2019-12-07.
+//  Copyright © 2019 Joacim Nidén. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
